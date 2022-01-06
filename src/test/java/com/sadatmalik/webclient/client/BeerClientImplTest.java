8bf2b808115f41eadc8baa60e031746c0c2edfb7
sadat.malik <sadat.malik@me.com>
2022-01-06 10:24:10 +0000

    Implementing test to handle expcetion case on delete

--- src/test/java/com/sadatmalik/webclient/client/BeerClientImplTest.java
+++ src/test/java/com/sadatmalik/webclient/client/BeerClientImplTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
@@ -15,6 +16,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class BeerClientImplTest {
 
@@ -148,6 +150,30 @@ class BeerClientImplTest {
         Mono<ResponseEntity<Void>> responseEntityMono = beerClient.updateBeerById(beer.getId(), updatedBeer);
         ResponseEntity<Void> responseEntity = responseEntityMono.block();
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
+
+    @Test
+    void deleteBeerByIdHandleException() {
+        Mono<ResponseEntity<Void>> responseEntityMono = beerClient.deleteBeerById(UUID.randomUUID());
+        ResponseEntity<Void> responseEntity = responseEntityMono.onErrorResume(throwable -> {
+            if (throwable instanceof WebClientResponseException) {
+                WebClientResponseException exception = (WebClientResponseException) throwable;
+                return Mono.just(ResponseEntity.status(exception.getStatusCode()).build());
+            } else {
+                throw new RuntimeException(throwable);
+            }
+        }).block();
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+        @Test
+    void deleteBeerByIdNotFound() {
+        Mono<ResponseEntity<Void>> responseEntityMono = beerClient.deleteBeerById(UUID.randomUUID());
+        assertThrows(WebClientResponseException.class, () -> {
+            ResponseEntity<Void> responseEntity = responseEntityMono.block();
+            assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        });
     }
 
     @Test
